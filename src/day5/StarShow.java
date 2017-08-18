dc527f870c4320ea40353de1ab4fa36d6526c67a
ThomasTwomey <league@WTS15.attlocal.net>
2017-08-18 14:54:37 -0700

    Scary

--- src/day5/StarShow.java
+++ src/day5/StarShow.java
@@ -46,9 +46,12 @@ public class StarShow {
 
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+robot.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
-
+for (int i = 0; i < 5; i++) {
+	robot.move(starSize);
+	robot.turn(144);
+}
 			// 1. Move the robot the distance of the starSize variable
 	
 			// 3. Turn the robot 144 degrees
